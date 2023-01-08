5c482cec4922a6e0c1a133f7f27214bc1166a79b
theTiv <justin.gluck@vibe.co.com>
2023-01-08 17:21:51 +0000

    Added Alarm bells and ringer assets, added new functions for set Alarm time, Play Alarm, Turn off Alarm.

--- src/clock.js
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-import Clock from "./pages/clock.html";
-import "./style.scss";
-
-function init() {
-  alert("Hi Justin!");
-}
-
-init();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from "lodash";
-import Clock from "./pages/clock.html";
+import Clock from "./ui/clock-face.html";
+import Alarm from "./ui/alarm-control.html";
+import AlarmBells from "./ui/alarm-bells.html";
 import "./style.scss";
 
-class init {
-  constructor() {
-    document.body.appendChild(this.render());
-    this.setTime(); // set time on load
-    setInterval(this.setTime.bind(this), 1000);
+class AlarmClock {
+  constructor(alarmTime = null) {
+    this.alarmTime = alarmTime; // time to set alarm
+    console.log(this.alarmTime);
+    document.body.appendChild(this.render()); // add clock elements to DOM
+    this.updateTime(); // set time on load
+    setInterval(this.updateTime.bind(this), 1000); // update time every second
+    this.addEventListeners(); // add event listeners
+    this.leftBell = document.querySelector(".alarm-bells__bell--left");
+    this.rightBell = document.querySelector(".alarm-bells__bell--right");
   }
 
   render() {
@@ -14,27 +21,74 @@ class init {
 
     wrapper.setAttribute("id", "clock");
     wrapper.innerHTML = Clock;
+    wrapper.insertAdjacentHTML("afterbegin", AlarmBells);
+    wrapper.insertAdjacentHTML("beforeend", Alarm);
 
     return wrapper;
   }
 
-  setTime() {
-    const hourshand = document.querySelector(".clock-hands__hours");
-    const minuteshand = document.querySelector(".clock-hands__minutes");
-    const secondshand = document.querySelector(".clock-hands__seconds");
+  updateTime() {
+    const hoursHand = document.querySelector(".clock-hands__hours");
+    const minutesHand = document.querySelector(".clock-hands__minutes");
+    const secondsHand = document.querySelector(".clock-hands__seconds");
+
     const time = new Date();
     const seconds = time.getSeconds() / 60;
     const minutes = (seconds + time.getMinutes()) / 60;
     const hours = (minutes + time.getHours()) / 12;
+    const digitalTime = time.toLocaleTimeString([], { timeStyle: "short" });
 
-    this.setRotation(secondshand, seconds);
-    this.setRotation(minuteshand, minutes);
-    this.setRotation(hourshand, hours);
+    this.setRotation(secondsHand, seconds);
+    this.setRotation(minutesHand, minutes);
+    this.setRotation(hoursHand, hours);
+    console.log("alarmTimes updateTime");
+    console.log(digitalTime);
+    console.log(this.alarmTime);
+    if (digitalTime == this.alarmTime) {
+      this.playAlarm();
+    }
   }
 
   setRotation(element, rotationRatio) {
     element.style.setProperty("--rotation", rotationRatio * 360);
   }
+
+  addEventListeners() {
+    document
+      .querySelector("#set_alarm")
+      .addEventListener("click", (e) => this.setAlarmTime(e));
+
+    document
+      .querySelector("#alarm_off")
+      .addEventListener("click", (e) => this.turnOffAlarm(e));
+  }
+
+  setAlarmTime() {
+    const alarmTime = document.querySelector(".alarm-control__input").value;
+    // console.log(alarmTime);
+    this.alarmTime = alarmTime;
+    document.querySelector("#set_alarm").value = `Alarm Set`;
+    document.querySelector(
+      "#alarm_status"
+    ).innerText = `Alarm time is set for ${alarmTime}`;
+  }
+
+  playAlarm() {
+    const audio = document.querySelector("#alarm_audio");
+    audio.play();
+    document.querySelector("#alarm_status").innerText = `Alarm is ringing`;
+    this.leftBell.classList.add("bell-ringing");
+    this.rightBell.classList.add("bell-ringing");
+  }
+
+  turnOffAlarm() {
+    console.log("Alarm off");
+    this.alarmTime = null;
+    document.querySelector("#set_alarm").value = "Set Alarm";
+    document.querySelector("#alarm_status").innerText = `The Alarm is now off`;
+    this.leftBell.classList.remove("bell-ringing");
+    this.rightBell.classList.remove("bell-ringing");
+  }
 }
 
-new init();
+new AlarmClock();
